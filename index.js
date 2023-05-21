2977b952c2ef9e25a3b704e98458ca9253ccae5f
João Ribeiro <102440368+joaoevribas@users.noreply.github.com>
2023-05-21 01:56:28 +0100

    Add firebase

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 "use strict";
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL:
+    "https://we-are-the-champions-scrimba-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const endorsementsInDB = ref(database, "endorsements");
